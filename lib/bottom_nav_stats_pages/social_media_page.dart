2284cb48fdd5fd4ba9f7fb7af14790a652f0cdbd
David Ogundepo <david.oludepo@gmail.com>
2023-01-01 03:17:39 +0000

    Adding Social Media Page

---
+++ lib/bottom_nav_stats_pages/social_media_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaPage extends StatefulWidget {
+  const SocialMediaPage({Key? key}) : super(key: key);
+
+  @override
+  State<SocialMediaPage> createState() => _SocialMediaPageState();
+}
+
+class _SocialMediaPageState extends State<SocialMediaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(),
+    );
+  }
+}
