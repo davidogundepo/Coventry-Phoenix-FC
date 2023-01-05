5557efe44978c5e140ec21c58435171afe771691
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-01-05 10:55:58 +0000

    Embedding Twitter Timeline == Very close

--- lib/bottom_nav_stats_pages/social_media_page.dart
+++ lib/bottom_nav_stats_pages/social_media_page.dart
@@ -1,24 +1,68 @@
+
 import 'package:flutter/material.dart';
+import 'package:twitter_oembed_api/twitter_oembed_api.dart';
+import 'dart:developer';
+import 'package:flutter_html/flutter_html.dart';
 
-class SocialMediaPage extends StatefulWidget {
-  const SocialMediaPage({Key? key}) : super(key: key);
 
-  @override
-  State<SocialMediaPage> createState() => _SocialMediaPageState();
+dynamic embeddedTimeline;
+
+
+void main() async {
+  final twitterApi = TwitterOEmbedApi();
+
+  try {
+    /// You can get the embedded tweet by specifying the tweet ID.
+    // final embeddedTweet = await twitterApi.publishEmbeddedTweet(
+    //   screenName: 'Interior',
+    //   tweetId: '507185938620219395',
+    //   maxWidth: 550,
+    //   align: ContentAlign.center,
+    // );
+    //
+    // // print(embeddedTweet.html);
+    // log(embeddedTweet.html);
+
+    /// You can get the embedded timeline by specifying the screen name,
+    /// or with list name.
+
+
+    embeddedTimeline = await twitterApi.publishEmbeddedTimeline(
+      screenName: 'DavidOOludepo',
+      // limit: 3,
+      theme: ContentTheme.dark,
+    );
+
+    log(embeddedTimeline.html);
+
+
+  } on TwitterOEmbedException catch (e) {
+    print(e);
+  }
 }
 
-class _SocialMediaPageState extends State<SocialMediaPage> {
+class Hello extends StatefulWidget {
+  Hello({Key? key}) : super(key: key);
+
+  @override
+  State<Hello> createState() => _HelloState();
+
+
+}
+
+var htmlData = embeddedTimeline;
+
+class _HelloState extends State<Hello> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
-      body: Column(
-        children: const [
-          Card(),
-          Card()
-        ],
+      body: SingleChildScrollView(
+        child: Html(
+          data: htmlData,
+        ),
       ),
     );
   }
