087a3f0eec47a95e4c1ec2dc3bcaadef67fa9795
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-02 22:24:25 +0000

    Production Ready = 80%

--- lib/bottom_nav_stats_pages/bottom_navigator.dart
+++ lib/bottom_nav_stats_pages/bottom_navigator.dart
@@ -1,4 +1,3 @@
-
 import 'package:coventry_phoenix_fc/bottom_nav_stats_pages/social_media_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,26 +28,28 @@ import 'season_timeline_page.dart';
 import 'trainings_games_reels_page.dart';
 import 'package:provider/provider.dart';
 
-
-
 late TrainingsAndGamesReelsNotifier trainingsAndGamesReelsNotifier;
 late PlayerOfTheMonthStatsAndInfoNotifier playerOfTheMonthStatsAndInfoNotifier;
-late MostFouledYCPlayersStatsAndInfoNotifier mostFouledYCPlayersStatsAndInfoNotifier;
-late MostFouledRCPlayersStatsAndInfoNotifier mostFouledRCPlayersStatsAndInfoNotifier;
+late MostFouledYCPlayersStatsAndInfoNotifier
+    mostFouledYCPlayersStatsAndInfoNotifier;
+late MostFouledRCPlayersStatsAndInfoNotifier
+    mostFouledRCPlayersStatsAndInfoNotifier;
 late TopGKPlayersStatsAndInfoNotifier topGKPlayersStatsAndInfoNotifier;
-late TopDefensivePlayersStatsAndInfoNotifier topDefensivePlayersStatsAndInfoNotifier;
+late TopDefensivePlayersStatsAndInfoNotifier
+    topDefensivePlayersStatsAndInfoNotifier;
 late TopGoalsPlayersStatsAndInfoNotifier topGoalsPlayersStatsAndInfoNotifier;
-late MostAssistsPlayersStatsAndInfoNotifier mostAssistsPlayersStatsAndInfoNotifier;
+late MostAssistsPlayersStatsAndInfoNotifier
+    mostAssistsPlayersStatsAndInfoNotifier;
 late MOTMPlayersStatsAndInfoNotifier motmPlayersStatsAndInfoNotifier;
 late CumMOTMPlayersStatsAndInfoNotifier cumMOTMPlayersStatsAndInfoNotifier;
 
 Color? backgroundColor = const Color.fromRGBO(34, 40, 49, 1);
-Color? cardBackgroundColorTwo = const Color.fromRGBO(34, 40, 49, 0.611764705882353);
+Color? cardBackgroundColorTwo =
+    const Color.fromRGBO(34, 40, 49, 0.611764705882353);
 Color? cardBackgroundColor = const Color.fromRGBO(57, 62, 70, 1);
 Color? goalsScoredTextColor = const Color.fromRGBO(255, 141, 41, 1);
 Color? appBarIconColor = const Color.fromRGBO(255, 141, 41, 1);
 Color? appBarBackgroundColor = const Color.fromRGBO(34, 40, 49, 1);
-
 
 class BottomNavigator extends StatefulWidget {
   const BottomNavigator({Key? key}) : super(key: key);
@@ -58,7 +59,6 @@ class BottomNavigator extends StatefulWidget {
 }
 
 class _BottomNavigatorState extends State<BottomNavigator> {
-
   bool toggle = false;
   int selectedPage = 0;
 
@@ -68,24 +68,16 @@ class _BottomNavigatorState extends State<BottomNavigator> {
     // const SeasonTimeline(),
     const SocialMediaPage(),
     TrainingsAndGamesReelsPage(),
-
   ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: _pageOption[selectedPage],
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withOpacity(.1)
-            )
-          ]
-        ),
+        decoration: BoxDecoration(color: backgroundColor, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        ]),
         child: SafeArea(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -93,7 +85,8 @@ class _BottomNavigatorState extends State<BottomNavigator> {
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
                   child: GNav(
                     tabBorderRadius: 10,
                     rippleColor: Colors.blueGrey,
@@ -101,27 +94,27 @@ class _BottomNavigatorState extends State<BottomNavigator> {
                     gap: 12,
                     activeColor: Colors.black,
                     iconSize: 24,
-                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 12),
                     duration: const Duration(microseconds: 400),
                     tabBackgroundColor: (cardBackgroundColor)!,
                     color: Colors.black,
                     haptic: true,
                     tabs: [
                       const GButton(
-                        gap: 12,
-                        icon: Icons.query_stats,
-                        textColor: Color.fromRGBO(247, 246, 242, 1),
-                        iconColor: Colors.white30,
-                        iconActiveColor: Color.fromRGBO(247, 246, 242, 1),
-                        rippleColor: Colors.blueGrey,
-                        text: 'A.P.T.'
-                      ),
-                      const GButton(
                           gap: 12,
-                        icon: Icons.workspace_premium,
+                          icon: Icons.query_stats,
                           textColor: Color.fromRGBO(247, 246, 242, 1),
                           iconColor: Colors.white30,
                           iconActiveColor: Color.fromRGBO(247, 246, 242, 1),
+                          rippleColor: Colors.blueGrey,
+                          text: 'A.P.T.'),
+                      const GButton(
+                        gap: 12,
+                        icon: Icons.workspace_premium,
+                        textColor: Color.fromRGBO(247, 246, 242, 1),
+                        iconColor: Colors.white30,
+                        iconActiveColor: Color.fromRGBO(247, 246, 242, 1),
                         text: 'Top Players',
                         rippleColor: Colors.blueGrey,
                       ),
@@ -148,15 +141,13 @@ class _BottomNavigatorState extends State<BottomNavigator> {
                         icon: Icons.view_carousel_outlined,
                         textColor: const Color.fromRGBO(255, 141, 41, 0.7),
                         iconColor: Colors.white30,
-                        iconActiveColor: const Color.fromRGBO(255, 141, 41, 0.7),
+                        iconActiveColor:
+                            const Color.fromRGBO(255, 141, 41, 0.7),
                         text: 'Reels',
                         activeBorder: Border.all(
                             color: const Color.fromRGBO(255, 141, 41, 0.7),
-                            width: 1
-                        ),
-                        border: Border.all(
-                          color: Colors.transparent
-                        ),
+                            width: 1),
+                        border: Border.all(color: Colors.transparent),
                         rippleColor: Colors.white30,
                       ),
                     ],
@@ -178,37 +169,51 @@ class _BottomNavigatorState extends State<BottomNavigator> {
 
   @override
   void initState() {
-
-    trainingsAndGamesReelsNotifier = Provider.of<TrainingsAndGamesReelsNotifier>(context, listen: false);
+    trainingsAndGamesReelsNotifier =
+        Provider.of<TrainingsAndGamesReelsNotifier>(context, listen: false);
     getTrainingsAndGamesReels(trainingsAndGamesReelsNotifier);
 
-    playerOfTheMonthStatsAndInfoNotifier = Provider.of<PlayerOfTheMonthStatsAndInfoNotifier>(context, listen: false);
+    playerOfTheMonthStatsAndInfoNotifier =
+        Provider.of<PlayerOfTheMonthStatsAndInfoNotifier>(context,
+            listen: false);
     getPlayerOfTheMonthStatsAndInfo(playerOfTheMonthStatsAndInfoNotifier);
 
-    mostFouledYCPlayersStatsAndInfoNotifier = Provider.of<MostFouledYCPlayersStatsAndInfoNotifier>(context, listen: false);
+    mostFouledYCPlayersStatsAndInfoNotifier =
+        Provider.of<MostFouledYCPlayersStatsAndInfoNotifier>(context,
+            listen: false);
     getMostFouledYCPlayersStatsAndInfo(mostFouledYCPlayersStatsAndInfoNotifier);
 
-    mostFouledRCPlayersStatsAndInfoNotifier = Provider.of<MostFouledRCPlayersStatsAndInfoNotifier>(context, listen: false);
+    mostFouledRCPlayersStatsAndInfoNotifier =
+        Provider.of<MostFouledRCPlayersStatsAndInfoNotifier>(context,
+            listen: false);
     getMostFouledRCPlayersStatsAndInfo(mostFouledRCPlayersStatsAndInfoNotifier);
 
-    topGKPlayersStatsAndInfoNotifier = Provider.of<TopGKPlayersStatsAndInfoNotifier>(context, listen: false);
+    topGKPlayersStatsAndInfoNotifier =
+        Provider.of<TopGKPlayersStatsAndInfoNotifier>(context, listen: false);
     getTopGKPlayersStatsAndInfo(topGKPlayersStatsAndInfoNotifier);
 
-    topDefensivePlayersStatsAndInfoNotifier = Provider.of<TopDefensivePlayersStatsAndInfoNotifier>(context, listen: false);
+    topDefensivePlayersStatsAndInfoNotifier =
+        Provider.of<TopDefensivePlayersStatsAndInfoNotifier>(context,
+            listen: false);
     getTopDefensivePlayersStatsAndInfo(topDefensivePlayersStatsAndInfoNotifier);
 
-    motmPlayersStatsAndInfoNotifier = Provider.of<MOTMPlayersStatsAndInfoNotifier>(context, listen: false);
+    motmPlayersStatsAndInfoNotifier =
+        Provider.of<MOTMPlayersStatsAndInfoNotifier>(context, listen: false);
     getMOTMPlayersStatsAndInfo(motmPlayersStatsAndInfoNotifier);
 
-    cumMOTMPlayersStatsAndInfoNotifier = Provider.of<CumMOTMPlayersStatsAndInfoNotifier>(context, listen: false);
+    cumMOTMPlayersStatsAndInfoNotifier =
+        Provider.of<CumMOTMPlayersStatsAndInfoNotifier>(context, listen: false);
     getCumMOTMPlayersStatsAndInfo(cumMOTMPlayersStatsAndInfoNotifier);
 
-    topGoalsPlayersStatsAndInfoNotifier = Provider.of<TopGoalsPlayersStatsAndInfoNotifier>(context, listen: false);
+    topGoalsPlayersStatsAndInfoNotifier =
+        Provider.of<TopGoalsPlayersStatsAndInfoNotifier>(context,
+            listen: false);
     getTopGoalsPlayersStatsAndInfo(topGoalsPlayersStatsAndInfoNotifier);
 
-    mostAssistsPlayersStatsAndInfoNotifier = Provider.of<MostAssistsPlayersStatsAndInfoNotifier>(context, listen: false);
+    mostAssistsPlayersStatsAndInfoNotifier =
+        Provider.of<MostAssistsPlayersStatsAndInfoNotifier>(context,
+            listen: false);
     getMostAssistsPlayersStatsAndInfo(mostAssistsPlayersStatsAndInfoNotifier);
-
 
     super.initState();
 
@@ -216,8 +221,5 @@ class _BottomNavigatorState extends State<BottomNavigator> {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-
   }
-
-
 }
