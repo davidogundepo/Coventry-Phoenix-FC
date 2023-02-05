8fae03277b48c249c9f5c0c31e5f179b12fc6b7b
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-05 22:43:46 +0000

    Production Ready = 82.5%

--- lib/notifier/founders_reviews_comment_notifier.dart
+++ lib/notifier/founders_reviews_comment_notifier.dart
@@ -1,25 +1,27 @@
-
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import '../model/founders_reviews_comment.dart';
 
-
 class FoundersReviewsCommentNotifier with ChangeNotifier {
-  List<FoundersReviewsComment> _foundersReviewsCommentList = <FoundersReviewsComment>[];
+  List<FoundersReviewsComment> _foundersReviewsCommentList =
+      <FoundersReviewsComment>[];
   late FoundersReviewsComment _currentFoundersReviewsComment;
 
-  UnmodifiableListView<FoundersReviewsComment> get foundersReviewsCommentList => UnmodifiableListView(_foundersReviewsCommentList);
+  UnmodifiableListView<FoundersReviewsComment> get foundersReviewsCommentList =>
+      UnmodifiableListView(_foundersReviewsCommentList);
 
-  FoundersReviewsComment get currentFoundersReviewsComment => _currentFoundersReviewsComment;
+  FoundersReviewsComment get currentFoundersReviewsComment =>
+      _currentFoundersReviewsComment;
 
-  set foundersReviewsCommentList(List<FoundersReviewsComment> foundersReviewsCommentList) {
+  set foundersReviewsCommentList(
+      List<FoundersReviewsComment> foundersReviewsCommentList) {
     _foundersReviewsCommentList = foundersReviewsCommentList;
     notifyListeners();
   }
 
-  set currentFoundersReviewsComment(FoundersReviewsComment foundersReviewsComment) {
+  set currentFoundersReviewsComment(
+      FoundersReviewsComment foundersReviewsComment) {
     _currentFoundersReviewsComment = foundersReviewsComment;
     notifyListeners();
   }
-
 }
