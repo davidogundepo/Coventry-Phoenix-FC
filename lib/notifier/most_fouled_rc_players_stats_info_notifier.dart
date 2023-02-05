5b687ae525fbd8aa16dbc193aa6fd8f8071c658f
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-05 22:44:34 +0000

    Production Ready = 82.5%

--- lib/notifier/most_fouled_rc_players_stats_info_notifier.dart
+++ lib/notifier/most_fouled_rc_players_stats_info_notifier.dart
@@ -1,25 +1,28 @@
-
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import '../model/players_stats_and_info.dart';
 
-
 class MostFouledRCPlayersStatsAndInfoNotifier with ChangeNotifier {
-  List<PlayersStatsAndInfo> _mostFouledRCPlayersStatsAndInfoList = <PlayersStatsAndInfo>[];
+  List<PlayersStatsAndInfo> _mostFouledRCPlayersStatsAndInfoList =
+      <PlayersStatsAndInfo>[];
   late PlayersStatsAndInfo _currentMostFouledRCPlayersStatsAndInfo;
 
-  UnmodifiableListView<PlayersStatsAndInfo> get mostFouledRCPlayersStatsAndInfoList => UnmodifiableListView(_mostFouledRCPlayersStatsAndInfoList);
+  UnmodifiableListView<PlayersStatsAndInfo>
+      get mostFouledRCPlayersStatsAndInfoList =>
+          UnmodifiableListView(_mostFouledRCPlayersStatsAndInfoList);
 
-  PlayersStatsAndInfo get currentMostFouledRCPlayersStatsAndInfo => _currentMostFouledRCPlayersStatsAndInfo;
+  PlayersStatsAndInfo get currentMostFouledRCPlayersStatsAndInfo =>
+      _currentMostFouledRCPlayersStatsAndInfo;
 
-  set mostFouledRCPlayersStatsAndInfoList(List<PlayersStatsAndInfo> mostFouledRCPlayersStatsAndInfoList) {
+  set mostFouledRCPlayersStatsAndInfoList(
+      List<PlayersStatsAndInfo> mostFouledRCPlayersStatsAndInfoList) {
     _mostFouledRCPlayersStatsAndInfoList = mostFouledRCPlayersStatsAndInfoList;
     notifyListeners();
   }
 
-  set currentMostFouledRCPlayersStatsAndInfo(PlayersStatsAndInfo playersStatsAndInfo) {
+  set currentMostFouledRCPlayersStatsAndInfo(
+      PlayersStatsAndInfo playersStatsAndInfo) {
     _currentMostFouledRCPlayersStatsAndInfo = playersStatsAndInfo;
     notifyListeners();
   }
-
 }
