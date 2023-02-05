f0e16cc8284a19721fd73d02bf31a28621ea80a7
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-05 22:44:09 +0000

    Production Ready = 82.5%

--- lib/notifier/player_of_the_month_stats_info_notifier.dart
+++ lib/notifier/player_of_the_month_stats_info_notifier.dart
@@ -1,25 +1,28 @@
-
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import '../model/players_stats_and_info.dart';
 
-
 class PlayerOfTheMonthStatsAndInfoNotifier with ChangeNotifier {
-  List<PlayersStatsAndInfo> _playerOfTheMonthStatsAndInfoList = <PlayersStatsAndInfo>[];
+  List<PlayersStatsAndInfo> _playerOfTheMonthStatsAndInfoList =
+      <PlayersStatsAndInfo>[];
   PlayersStatsAndInfo? _currentPlayerOfTheMonthStatsAndInfo;
 
-  UnmodifiableListView<PlayersStatsAndInfo> get playerOfTheMonthStatsAndInfoList => UnmodifiableListView(_playerOfTheMonthStatsAndInfoList);
+  UnmodifiableListView<PlayersStatsAndInfo>
+      get playerOfTheMonthStatsAndInfoList =>
+          UnmodifiableListView(_playerOfTheMonthStatsAndInfoList);
 
-  PlayersStatsAndInfo? get currentPlayerOfTheMonthStatsAndInfo => _currentPlayerOfTheMonthStatsAndInfo;
+  PlayersStatsAndInfo? get currentPlayerOfTheMonthStatsAndInfo =>
+      _currentPlayerOfTheMonthStatsAndInfo;
 
-  set playerOfTheMonthStatsAndInfoList(List<PlayersStatsAndInfo> playerOfTheMonthStatsAndInfoList) {
+  set playerOfTheMonthStatsAndInfoList(
+      List<PlayersStatsAndInfo> playerOfTheMonthStatsAndInfoList) {
     _playerOfTheMonthStatsAndInfoList = playerOfTheMonthStatsAndInfoList;
     notifyListeners();
   }
 
-  set currentPlayerOfTheMonthStatsAndInfo(PlayersStatsAndInfo? playersStatsAndInfo) {
+  set currentPlayerOfTheMonthStatsAndInfo(
+      PlayersStatsAndInfo? playersStatsAndInfo) {
     _currentPlayerOfTheMonthStatsAndInfo = playersStatsAndInfo;
     notifyListeners();
   }
-
 }
