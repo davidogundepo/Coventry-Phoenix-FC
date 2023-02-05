f498296cd9ab28831d1ed6271f0cdf29789545ce
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-05 22:43:08 +0000

    Production Ready = 82.5%

--- lib/notifier/coaches_reviews_comment_notifier.dart
+++ lib/notifier/coaches_reviews_comment_notifier.dart
@@ -1,25 +1,27 @@
-
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import '../model/coaches_reviews_comment.dart';
 
-
 class CoachesReviewsCommentNotifier with ChangeNotifier {
-  List<CoachesReviewsComment> _coachesReviewsCommentList = <CoachesReviewsComment>[];
+  List<CoachesReviewsComment> _coachesReviewsCommentList =
+      <CoachesReviewsComment>[];
   late CoachesReviewsComment _currentCoachesReviewsComment;
 
-  UnmodifiableListView<CoachesReviewsComment> get coachesReviewsCommentList => UnmodifiableListView(_coachesReviewsCommentList);
+  UnmodifiableListView<CoachesReviewsComment> get coachesReviewsCommentList =>
+      UnmodifiableListView(_coachesReviewsCommentList);
 
-  CoachesReviewsComment get currentCoachesReviewsComment => _currentCoachesReviewsComment;
+  CoachesReviewsComment get currentCoachesReviewsComment =>
+      _currentCoachesReviewsComment;
 
-  set coachesReviewsCommentList(List<CoachesReviewsComment> coachesReviewsCommentList) {
+  set coachesReviewsCommentList(
+      List<CoachesReviewsComment> coachesReviewsCommentList) {
     _coachesReviewsCommentList = coachesReviewsCommentList;
     notifyListeners();
   }
 
-  set currentCoachesReviewsComment(CoachesReviewsComment coachesReviewsComment) {
+  set currentCoachesReviewsComment(
+      CoachesReviewsComment coachesReviewsComment) {
     _currentCoachesReviewsComment = coachesReviewsComment;
     notifyListeners();
   }
-
 }
