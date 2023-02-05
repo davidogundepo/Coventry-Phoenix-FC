a4751d210eb2020f9c8cf281e8266c14aa393e74
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-05 22:43:20 +0000

    Production Ready = 82.5%

--- lib/notifier/cum_motm_players_stats_info_notifier.dart
+++ lib/notifier/cum_motm_players_stats_info_notifier.dart
@@ -1,25 +1,28 @@
-
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import '../model/players_stats_and_info.dart';
 
-
 class CumMOTMPlayersStatsAndInfoNotifier with ChangeNotifier {
-  List<PlayersStatsAndInfo> _cumMOTMPlayersStatsAndInfoList = <PlayersStatsAndInfo>[];
+  List<PlayersStatsAndInfo> _cumMOTMPlayersStatsAndInfoList =
+      <PlayersStatsAndInfo>[];
   late PlayersStatsAndInfo _currentCumMOTMPlayersStatsAndInfo;
 
-  UnmodifiableListView<PlayersStatsAndInfo> get cumMOTMPlayersStatsAndInfoList => UnmodifiableListView(_cumMOTMPlayersStatsAndInfoList);
+  UnmodifiableListView<PlayersStatsAndInfo>
+      get cumMOTMPlayersStatsAndInfoList =>
+          UnmodifiableListView(_cumMOTMPlayersStatsAndInfoList);
 
-  PlayersStatsAndInfo get currentCumMOTMPlayersStatsAndInfo => _currentCumMOTMPlayersStatsAndInfo;
+  PlayersStatsAndInfo get currentCumMOTMPlayersStatsAndInfo =>
+      _currentCumMOTMPlayersStatsAndInfo;
 
-  set cumMOTMPlayersStatsAndInfoList(List<PlayersStatsAndInfo> cumMOTMPlayersStatsAndInfoList) {
+  set cumMOTMPlayersStatsAndInfoList(
+      List<PlayersStatsAndInfo> cumMOTMPlayersStatsAndInfoList) {
     _cumMOTMPlayersStatsAndInfoList = cumMOTMPlayersStatsAndInfoList;
     notifyListeners();
   }
 
-  set currentCumMOTMPlayersStatsAndInfo(PlayersStatsAndInfo playersStatsAndInfo) {
+  set currentCumMOTMPlayersStatsAndInfo(
+      PlayersStatsAndInfo playersStatsAndInfo) {
     _currentCumMOTMPlayersStatsAndInfo = playersStatsAndInfo;
     notifyListeners();
   }
-
 }
