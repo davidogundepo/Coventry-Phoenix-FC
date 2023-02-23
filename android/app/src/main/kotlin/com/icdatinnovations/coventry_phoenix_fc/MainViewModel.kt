f473d6854f48b22a38db8c58228be6e3ab9e1d25
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-02-23 13:46:44 +0000

    Production Ready = 99.7%

--- android/app/src/main/kotlin/com/icdatinnovations/coventry_phoenix_fc/MainViewModel.kt
+++ android/app/src/main/kotlin/com/icdatinnovations/coventry_phoenix_fc/MainViewModel.kt
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-internal class MainViewModel(context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
+internal class MainViewModel(context: Context, id: Int) : PlatformView {
 
     private val textView: TextView = TextView(context)
 
@@ -21,14 +21,16 @@ internal class MainViewModel(context: Context, id: Int, creationParams: Map<Stri
     init {
         textView.textSize = 32f
         textView.setBackgroundColor(Color.rgb(255, 255, 255))
-        textView.text = "Rendered on a native Android view (id: $id)"
+        val text = context.getString(R.string.native_view_text, id)
+        textView.text = text
     }
 }
 
- class DemoNativeViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class DemoNativeViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context?, id: Int, args: Any?): PlatformView {
-        val creationParams = args as Map<String?, Any?>?
-        return MainViewModel(context!!, id, creationParams)
+        args as? Map<*, *> // Add type check
+        return MainViewModel(context!!, id)
     }
 }
+
 
--- android/app/src/main/kotlin/com/icdatinnovations/coventry_phoenix_fc/SplashView.kt
+++ android/app/src/main/kotlin/com/icdatinnovations/coventry_phoenix_fc/SplashView.kt
@@ -15,10 +15,13 @@ class SplashView : SplashScreen {
 
     private val nullParent: ViewGroup? = null
 
-    override fun createSplashView(context: Context, savedInstanceState: Bundle?): View? {
+    override fun createSplashView(context: Context, savedInstanceState: Bundle?): View {
         val rootView = FrameLayout(context)
         rootView.apply {
-            layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            layoutParams = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
 
             setBackgroundColor(Color.parseColor("#212529"))
             addView(LayoutInflater.from(context).inflate(R.layout.splash_view, nullParent, false))
