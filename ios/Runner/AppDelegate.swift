b120e202f7719c51f682bc47b6066d64a7e4db8c
David Ogundepo <ogundepo.david@lmu.edu.ng>
2023-03-15 21:38:23 +0000

    Production Ready = 99.8%

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,29 @@
 import UIKit
 import Flutter
 import Firebase
+import OneSignal
 //import UserNotifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate  {
+@objc class AppDelegate: UIResponder, UIApplicationDelegate, FlutterAppDelegate, MessagingDelegate  {
     
     lazy var flutterEngine = FlutterEngine(name: "MyApp")
     
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? ) -> Bool {
+      UIApplication.shared.isStatusBarHidden = false
       
-    UIApplication.shared.isStatusBarHidden = false
+      // Remove this method to stop OneSignal Debugging
+        OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+        
+        // OneSignal initialization
+        OneSignal.initWithLaunchOptions(launchOptions)
+        OneSignal.setAppId("6b1cda87-62bf-44d0-9243-9088805b7909")
+        
+        // promptForPushNotifications will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+          print("User accepted notifications: \(accepted)")
+        })
       
 //     (BOOL)prefersStatusBarHidden {
 //
